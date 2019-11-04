960d63087cc4bfa58b5896bfa4fbec6dceff57df
Lambda <lambda@Lambdas-MacBook-Pro-3.local>
2019-11-04 16:51:03 +0100

    attempted bft, dft

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,6 +3,19 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
+    # COMPARISON	STACK	                     QUEUE
+# Working principle	LIFO (Last in First out)	FIFO (First in First out)
+# Structure	Same end is used to insert and delete elements.	One end is used for insertion, i.e., rear end and another end is used for deletion of elements, i.e., front end.
+# Number of pointers used	One	                Two (In simple queue case)
+# Operations performed	Push and Pop	        Enqueue and dequeue
+# Examination of empty condition	Top == -1	Front == -1 || Front == Rear + 1
+# Examination of full condition
+# Top == Max - 1	                            Rear == Max - 1
+# Variants	It does not have variants.	        It has variants like circular queue, priority queue, doubly ended queue.
+# Implementation	                            Simpler	Comparatively complex
+
+# Breadth-First Search ( or Traversal) also know as Level Order Traversal.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
@@ -11,24 +24,68 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # if v1 and v2 exist in vertices list
+        if v1 in self.vertices and v2 in self.vertices:
+            # add v2 at v1 of vertices
+            # (add v2 to the vertices list at the index of v1)
+            self.vertices[v1].add(v2)
+        # otherwise
+        else:
+            # raise an error
+            raise KeyError("That vertex does not exist")
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # create an empty queue and enqueue the starting vertex ID
+        q = Queue()
+        # first in first out
+        q.enqueue(starting_vertex)
+        # create a set to store the visited vertices
+        visited = set()
+        # while the queue is not empty
+        while q.size() > 0:
+            # Dequeue the first vertex
+            v = q.dequeue()
+            # if that vertex has not been visited
+            if v not in visited:
+                # mark it as visited (printing for a representation)
+                print(v)
+                visited.add(v)
+                # then add all of it's neighbors to the back of the queue
+                for next_vertex in self.vertices[v]:
+                    q.enqueue(next_vertex)
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # create an empty stack and push the starting vertex ID
+        s = Stack()
+        # Last in last out
+        s.push(starting_vertex)
+        # create a set to store the visited vertices
+        visited = set()
+        # while the stack is not empty
+        while s.size() > 0:
+            # pop the first vertex
+            v = s.pop()
+            # if that vertex has not been visited
+            if v not in visited:
+                # mark it as visited (printing for a representation)
+                print(v)
+                visited.add(v)
+                # then add all of it's neighbors to the top of the stack
+                for next_vertex in self.vertices[v]:
+                    s.push(next_vertex)
+            # it push watever that is inside it and works with the stack
+            # then as it moves from one point to another the stacks add to itself
     def dft_recursive(self, starting_vertex):
         """
         Print each vertex in depth-first order
@@ -42,7 +99,27 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        q = Queue()
+        # FIFO
+        q.enqueue(starting_vertex)
+        # create a set to store the visited vertices
+        visited = set()
+        path = {}
+        # while the queue is not empty
+        while q.size() > 0:
+            # Dequeue the first vertex
+            v = q.dequeue()
+            # if that vertex has not been visited
+            if v not in visited:
+                # mark it as visited (printing for a representation)
+                # print(v)
+                visited.add(v)
+                # then add all of it's neighbors to the back of the queue
+                for next_vertex in self.vertices[v]:
+                    q.enqueue(next_vertex)
+                    
+
+
     def dfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing a path from
@@ -108,24 +185,27 @@ if __name__ == '__main__':
     '''
     graph.bft(1)
 
-    '''
-    Valid DFT recursive paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-    graph.dft_recursive(1)
+    # '''
+    # Valid DFT recursive paths:
+    #     1, 2, 3, 5, 4, 6, 7
+    #     1, 2, 3, 5, 4, 7, 6
+    #     1, 2, 4, 7, 6, 3, 5
+    #     1, 2, 4, 6, 3, 5, 7
+    # '''
+    # graph.dft_recursive(1)
 
-    '''
-    Valid BFS path:
-        [1, 2, 4, 6]
-    '''
-    print(graph.bfs(1, 6))
+    # '''
+    # Valid BFS path:
+    #     [1, 2, 4, 6]
+    # '''
+    # print(graph.bfs(1, 6))
 
-    '''
-    Valid DFS paths:
-        [1, 2, 4, 6]
-        [1, 2, 4, 7, 6]
-    '''
-    print(graph.dfs(1, 6))
+    # '''
+    # Valid DFS paths:
+    #     [1, 2, 4, 6]
+    #     [1, 2, 4, 7, 6]
+    # '''
+    # print(graph.dfs(1, 6))
+
+
+
