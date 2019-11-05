f7bd72052ae465556e29043b6e3b22e1368d6928
Lambda <lambda@Lambdas-MacBook-Pro-3.local>
2019-11-05 09:46:31 +0100

    bfs, dft_recursive

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -86,42 +86,97 @@ class Graph:
                     s.push(next_vertex)
             # it push watever that is inside it and works with the stack
             # then as it moves from one point to another the stacks add to itself
-    def dft_recursive(self, starting_vertex):
+    def dft_recursive(self, starting_vertex, visited = set()):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         This should be done using recursion.
         """
-        pass  # TODO
+        # if not starting_vertex:
+        #     return
+        print(starting_vertex)
+        visited.add(starting_vertex)
+        for i in self.vertices[starting_vertex]:
+            if i not in visited:
+              self.dft_recursive(i, visited)
+    
+
+
+
     def bfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing the shortest path from
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        q = Queue()
-        # FIFO
-        q.enqueue(starting_vertex)
-        # create a set to store the visited vertices
-        visited = set()
-        path = {}
-        # while the queue is not empty
-        while q.size() > 0:
-            # Dequeue the first vertex
-            v = q.dequeue()
-            # if that vertex has not been visited
-            if v not in visited:
-                # mark it as visited (printing for a representation)
-                # print(v)
-                visited.add(v)
-                # then add all of it's neighbors to the back of the queue
-                for next_vertex in self.vertices[v]:
-                    q.enqueue(next_vertex)
+        # vertices = self.vertices
+        # # maintain a queue of paths
+        # queue = []
+        # # push the first path into the queue
+        # queue.append([starting_vertex])
+        # while len(queue) > 0:
+        #     # get the first path from the queue
+        #     print(queue)
+        #     path = queue.pop(0)
+        #     print(queue)
+        #     # get the last node from the path
+        #     node = path[-1]
+        #     # path found
+        #     if node == destination_vertex:
+        #         return print(path)
+        #     # enumerate all adjacent nodes, construct a new path and push it into the queue
+        #     for adjacent in verts.get(node, []):
+        #         new_path = list(path)
+        #         new_path.append(adjacent)
+        #         queue.append(new_path)  
+        #         verts = self.vertices
+
+
+        # maintain a queue of paths
+        queue = Queue()
+        # push the first path into the queue
+        queue.enqueue([starting_vertex])
+        # forget about everything up from here
+        while queue.size() > 0:
+            # get the first path from the queue
+            path = queue.dequeue()
+            # major code hold the previous path ..then the other adds to it
+            # get the last node from the path
+            node = path[-1]
+            # path found
+            if node == destination_vertex:
+                return print(path)
+            # enumerate all adjacent nodes, construct a new path and push it into the queue
+            for i in self.vertices.get(node):
+                new_path = list(path)
+                new_path.append(i)
+                queue.enqueue(new_path)  
+
+
+        # q = Queue()
+        # # FIFO
+        # q.enqueue(starting_vertex)
+        # # create a set to store the visited vertices
+        # visited = set()
+        # path = {}
+        # # while the queue is not empty
+        # while q.size() > 0:
+        #     # Dequeue the first vertex
+        #     v = q.dequeue()
+        #     # if that vertex has not been visited
+        #     if v not in visited:
+        #         # mark it as visited (printing for a representation)
+        #         # print(v)
+        #         visited.add(v) 
+        #         # then add all of it's neighbors to the back of the queue
+        #         for next_vertex in self.vertices[v]:
+        #             q.enqueue(next_vertex)
                     
 
 
     def dfs(self, starting_vertex, destination_vertex):
         """
+        Dijkstra's shortest path
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
@@ -185,26 +240,26 @@ if __name__ == '__main__':
     '''
     graph.bft(1)
 
-    # '''
-    # Valid DFT recursive paths:
-    #     1, 2, 3, 5, 4, 6, 7
-    #     1, 2, 3, 5, 4, 7, 6
-    #     1, 2, 4, 7, 6, 3, 5
-    #     1, 2, 4, 6, 3, 5, 7
-    # '''
-    # graph.dft_recursive(1)
+    '''
+    Valid DFT recursive paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    graph.dft_recursive(1)
 
-    # '''
-    # Valid BFS path:
-    #     [1, 2, 4, 6]
-    # '''
-    # print(graph.bfs(1, 6))
+    '''
+    Valid BFS path:
+        [1, 2, 4, 6]
+    '''
+    print(graph.bfs(1, 6))
 
-    # '''
-    # Valid DFS paths:
-    #     [1, 2, 4, 6]
-    #     [1, 2, 4, 7, 6]
-    # '''
+    '''
+    Valid DFS paths:
+        [1, 2, 4, 6]
+        [1, 2, 4, 7, 6]
+    '''
     # print(graph.dfs(1, 6))
 
 
